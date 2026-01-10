e29ea52d85cab4128fb78bab6d5804b72e6e20cc
Andre Li <156480488+kandre41@users.noreply.github.com>
2026-01-10 17:38:02 -0500

    takeoff and land working

--- flight_control.py
+++ flight_control.py
@@ -7,23 +7,27 @@ import pandas as pd
 import os
 import joblib
 import numpy as np
+from DJITelloPy.djitellopy import tello
+import time
 # 1. Load the YOLO model 
 PATH=r"W:\VSCode\drone_project"
 
 
 #Set the Webcam Source
 # '0' typically refers to the default primary camera 
-WEBCAM_SOURCE = 1
+WEBCAM_SOURCE = 0
 
-
+drone=tello.Tello()
+drone.connect()
+time.sleep(15)
 def main():
     loaded_scaler = joblib.load(os.path.join(PATH,r"scaler\scaler.bin"))
     model = YOLO(os.path.join(PATH,r"weights\yolo11x-pose.engine"))
     mlp = PoseControlNet(num_controls=4)
     mlp.load_state_dict(torch.load(os.path.join(PATH,r"weights\pose_control_model.pt"), weights_only=True))
     mlp.eval()
-    #cap = cv2.VideoCapture(WEBCAM_SOURCE) #sets cap to the webcam source
-    cap=cv2.VideoCapture(os.path.join(PATH,r"datasets\videos\demo1.mp4"))
+    cap = cv2.VideoCapture(WEBCAM_SOURCE) #sets cap to the webcam source
+    #cap=cv2.VideoCapture(os.path.join(PATH,r"datasets\videos\demo1.mp4"))
     if not cap.isOpened():
         print("Error: Could not open webcam.")
         exit()
@@ -43,7 +47,7 @@ def main():
                 print(df['right_arm_angle'])
                 if df.loc[0,'left_elbow_angle']>-2.5:
                     if not take_off:
-                        #send command to drone to take off
+                        drone.takeoff()
                         take_off=True
                         pass
                     dist_cols=df.filter(like='len').columns
@@ -64,10 +68,11 @@ def main():
                     control_vector=mlp(result_tensor) #using cpu inference seems faster than transfering to and back from the gpu
 
                     print(control_vector)
-                    if df.loc[0,'right_arm_angle']<3.75 and df.loc[0,'right_arm_angle']>1:#typo the feature should be left_arm_angle, but i misnamed it to right_arm_angle
-                        #land
-                        pass
-
+                    if df.loc[0,'right_arm_angle']<3.75 and df.loc[0,'right_arm_angle']>1 and take_off:#typo the feature should be left_arm_angle, but i misnamed it to right_arm_angle
+                        take_off=False
+                        drone.land()
+                        time.sleep(4)
+                        
             annotated_frame = results[0].plot()
             cv2.imshow("drone control", annotated_frame)
             if cv2.waitKey(1) & 0xFF == ord("q"):
